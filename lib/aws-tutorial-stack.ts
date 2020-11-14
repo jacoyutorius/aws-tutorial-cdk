5335c3af8c27486c42f3b791fd5f78a2dc3e52b8
yutoogi <jacoyutorius@gmail.com>
2020-11-15 02:20:41 +0900

    VPC, SecurityGroup, InternetGateway, Subnet

---
+++ lib/aws-tutorial-stack.ts
@@ -0,0 +1,124 @@
+import * as cdk from '@aws-cdk/core';
+import {
+  DefaultInstanceTenancy,
+  RouterType,
+  Vpc,
+  Subnet,
+  CfnInternetGateway,
+  CfnVPCGatewayAttachment,
+  SecurityGroup,
+  Peer,
+  Port
+} from "@aws-cdk/aws-ec2";
+
+export class AwsTutorialStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const vpc = new Vpc(this, "cdk-vpc", {
+      cidr: "10.0.0.0/16",
+      defaultInstanceTenancy: DefaultInstanceTenancy.DEFAULT,
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
+      subnetConfiguration: []
+    });
+
+    const publicSubnet_1 = new Subnet(this, "PublicSubnet1a", {
+      availabilityZone: "ap-northeast-1a",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.10.0/24"
+    });
+    const publicSubnet_2 = new Subnet(this, "PublicSubnet1c", {
+      availabilityZone: "ap-northeast-1c",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.20.0/24"
+    });
+    const privateSubnet_1 = new Subnet(this, "PrivatSubnet1a", {
+      availabilityZone: "ap-northeast-1a",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.30.0/24"
+    });
+    const privateSubnet_2 = new Subnet(this, "PrivatSubnet1c", {
+      availabilityZone: "ap-northeast-1c",
+      vpcId: vpc.vpcId,
+      cidrBlock: "10.0.40.0/24"
+    });
+
+    const internetGateway = new CfnInternetGateway(this, "InternetGateway", {});
+    new CfnVPCGatewayAttachment(this, "gateway", {
+      vpcId: vpc.vpcId,
+      internetGatewayId: internetGateway.ref
+    });
+
+    publicSubnet_1.addRoute("PublicSubnet1Route", {
+      routerType: RouterType.GATEWAY,
+      routerId: internetGateway.ref
+    });
+    publicSubnet_2.addRoute("PublicSubnet2Route", {
+      routerType: RouterType.GATEWAY,
+      routerId: internetGateway.ref
+    });
+
+    const cidrIp = this.node.tryGetContext('cidr_ip');
+
+    // 踏み台サーバー
+    const sgBastion = new SecurityGroup(this, "sg-bastion", {
+      vpc,
+      allowAllOutbound: true,
+      description: "for bastion server",
+      securityGroupName: "bastion"
+    });
+    sgBastion.addIngressRule(
+      Peer.anyIpv4(), // とりあえずどこからでもアクセス許可とする
+      Port.tcp(80)
+    );
+
+    // Webサーバー
+    const sgWeb = new SecurityGroup(this, "sg-web", {
+      vpc,
+      allowAllOutbound: true,
+      description: "for web server",
+      securityGroupName: "web"
+    });
+    sgWeb.addIngressRule(
+      sgBastion,
+      Port.tcp(22)
+    );
+    sgWeb.addIngressRule(
+      sgBastion,
+      Port.tcp(22)
+    );
+
+    // Appサーバー
+    const sgApp = new SecurityGroup(this, "sg-app", {
+      vpc,
+      allowAllOutbound: true,
+      description: "for app server",
+      securityGroupName: "app"
+    });
+    sgApp.addIngressRule(
+      sgBastion,
+      Port.tcp(22)
+    );
+    sgApp.addIngressRule(
+      sgBastion,
+      Port.tcp(3000)
+    );
+
+    // DBサーバー
+    const sgDb = new SecurityGroup(this, "sg-db", {
+      vpc,
+      allowAllOutbound: true,
+      description: "for db server",
+      securityGroupName: "db"
+    });
+    sgDb.addIngressRule(
+      sgBastion,
+      Port.tcp(22)
+    );
+    sgDb.addIngressRule(
+      sgBastion,
+      Port.tcp(27017)
+    );
+  }
+}
---
+++ test/aws-tutorial.test.ts
@@ -0,0 +1,13 @@
+// import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import * as AwsTutorial from '../lib/aws-tutorial-stack';
+
+test('Empty Stack', () => {
+    // const app = new cdk.App();
+    // // WHEN
+    // const stack = new AwsTutorial.AwsTutorialStack(app, 'MyTestStack');
+    // // THEN
+    // expectCDK(stack).to(matchTemplate({
+    //   "Resources": {}
+    // }, MatchStyle.EXACT))
+});
